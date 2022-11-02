a11d07e47e85aca4c2bae5e4c758e2dee9887b75
Montana <montana@linux.com>
2022-11-02 10:10:10 -0700

    Docker Perforce Helix-Core Assembla Setups

---
+++ README.md
@@ -0,0 +1,7 @@
+# helixcore
+
+HelixCore ( Perforce ) 
+
+https://www.perforce.com/
+
+update 2022.1-2305383
---
+++ p4d-centos/files/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ ! -d "/tmp/check" ]; then
+
+mkdir /tmp/check
+echo `date` > /tmp/check/first_`date +%Y%m%d_%H%M%S`
+
+/opt/perforce/sbin/configure-helix-p4d.sh $P4NAME -n -p $P4PORT -r $P4ROOT -u $P4USER -P $P4PASSWD --case $CASE_INSENSITIVE --unicode
+
+cat > ~perforce/.p4config <<EOF
+P4USER=$P4USER
+P4PORT=$P4PORT
+P4PASSWD=$P4PASSWD
+EOF
+
+chmod 0600 ~perforce/.p4config
+chown perforce:perforce ~perforce/.p4config
+
+p4 login <<EOF
+$P4PASSWD
+EOF
+
+pushd /opt/perforce/servers/master
+rm -Rf db.*
+rm -Rf journal
+popd
+
+yes ${P4PASSWD} | p4 -p ${P4PORT} -u super login
+pushd /usr/local/bin/
+p4 triggers -o > triggers.txt
+echo '   CheckCaseTrigger change-submit //... "python3 /usr/local/bin/CheckCaseTrigger3.py %changelist% port=ssl:1666 user=super"' >> triggers.txt
+p4 triggers -i < triggers.txt
+popd
+
+else
+  echo `date` > /tmp/check/second_`date +%Y%m%d_%H%M%S`
+
+fi
+
+exec "$@"
